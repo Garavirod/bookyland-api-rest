58eccc1b32f3aa500595fcebbc3b595aae132119
Garavirod <rodrigogarciaavila26@gmail.com>
2024-06-26 15:21:58 -0600

    serve static index home api page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
-from database.db import create_db_connection
+from fastapi.staticfiles import StaticFiles
 from api import books, genres, authors
+from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 app = FastAPI()
+
+# Serve static content
+app.mount("/static",StaticFiles(directory='static'), name='static')
+
+
+# Serve the index.html file
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    index_file_path = Path("static/index.html")
+    return index_file_path.read_text()
 
 # Include router from endpoints
 app.include_router(books.router)
