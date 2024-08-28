753caba62e0542fb6f90da6b0bf531c0f9dc61c5
Garavirod <rodrigogarciaavila26@gmail.com>
2024-08-28 12:13:48 -0600

    feat(database): Add new configuration and .env.example

--- database/init.sql
+++ database/init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'bookyland_user_dev'@'%' IDENTIFIED BY 'bookyland0925';
+CREATE USER 'bookyland_user_dev'@'%' IDENTIFIED BY '@qwerqwerjklweriuyqwe87';
 GRANT ALL PRIVILEGES ON bookyland.* TO 'bookyland_user_dev'@'%';
 FLUSH PRIVILEGES;
