7a6ebb1a6f9ce37a3d0e38198867e9017a22f400
Garavirod <rodrigogarciaavila26@gmail.com>
2024-09-02 13:39:28 -0600

    feat(infra | setup | parameter-store): Add parameter store for db_password

--- infra/setup/parameter-store.tf
+++ infra/setup/parameter-store.tf
@@ -4,16 +4,16 @@
 
 // Random password
 resource "random_password" "database_user_password" {
-  length = 16
-  special = true
+  length           = 16
+  special          = true
   override_special = "!@#£$%^&*()-_=+[]{}<>:?"
 }
 
 // Secret Managaer definition
 resource "aws_ssm_parameter" "database_user_password" {
-  name = "${var.application_name}/database_user_password"
+  name        = "/${var.application_name}/database/user/password"
   description = "Randomly generated database user password"
-  type = "SecureString"
-  value = random_password.database_user_password.result
+  type        = "SecureString"
+  value       = random_password.database_user_password.result
 }
 
