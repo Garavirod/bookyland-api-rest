0222ea26e83f40c0ab5c3333a928ffd3f8dc419d
Garavirod <rodrigogarciaavila26@gmail.com>
2024-09-06 12:31:06 -0600

    feat(infra | setup | codepipeline): Add codepipeline and codebuild definitions

--- infra/setup/codebuild.tf
+++ infra/setup/codebuild.tf
@@ -1,19 +1,19 @@
-/* ########################
+########################
 # Codebuild definition #
 ########################
 resource "aws_codebuild_project" "codebuild" {
-  name = "${var.application_name}-codebuild"
+  name         = "${var.application_name}-codebuild"
   service_role = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
   }
-  
+
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:5.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
 
     environment_variable {
       name  = "TF_WORKSPACE"
@@ -21,24 +21,23 @@ resource "aws_codebuild_project" "codebuild" {
     }
 
     environment_variable {
-      name = "ECR_URI"
+      name  = "ECR_URI"
       value = aws_ecr_repository.app.repository_url
     }
 
     environment_variable {
-      name = "AWS_REGION"
+      name  = "AWS_REGION"
       value = "us-east-1"
     }
 
     environment_variable {
-      name = "SSM_PARAM_DB_PASSWORD_NAME"
+      name  = "SSM_PARAM_DB_PASSWORD_NAME"
       value = aws_ssm_parameter.database_user_password.name
     }
   }
 
   source {
-    type = "CODEPIPELINE"
+    type      = "CODEPIPELINE"
     buildspec = file("buildspec.yaml")
   }
 }
- */
--- infra/setup/codepipeline.tf
+++ infra/setup/codepipeline.tf
@@ -1,68 +1,87 @@
-/* ###########################
+###########################
 # CodePipeline Definition #
 ###########################
 resource "aws_codepipeline" "aws_codepipeline" {
-  name = "${var.application_name}-codepipeline"
+
+  name     = "${var.application_name}-codepipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
- # Stages definition
- stage {
-   name = "Source"
+
+  # Soruce stage for Dev
+  stage {
+    name = "Source_Dev"
     action {
-      name             = "Source"
+      name             = "Source_Dev"
       category         = "Source"
       owner            = "ThirdParty"
       provider         = "GitHub"
       version          = "1"
-      output_artifacts = ["source_output"]
+      output_artifacts = ["source_output_dev"]
 
       configuration = {
-        Owner  = var.github_user_name
-        Repo   = var.github_repository_name
-        Branch = var.github_branch_name
+        Owner      = var.github_user_name
+        Repo       = var.github_repository_name
+        Branch     = "dev"
         OAuthToken = var.github_oauthtoken
       }
-   }
- }
+    }
+  }
 
- stage {
-   name = "Build"
-   action {
-     name             = "Build"
+  # Source stage for prod
+  stage {
+    name = "Source_Prod"
+    action {
+      name             = "Source_Prod"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output_prod"]
+      configuration = {
+        Owner      = var.github_user_name
+        Repo       = var.github_repository_name
+        Branch     = "prod"
+        OAuthToken = var.github_oauthtoken
+      }
+    }
+  }
+
+  # Build stage for Dev
+  stage {
+    name = "Build_Dev"
+    action {
+      name             = "Build_Dev"
       category         = "Build"
       owner            = "AWS"
       provider         = "CodeBuild"
       version          = "1"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
+      input_artifacts  = ["source_output_dev"]
+      output_artifacts = ["build_output_dev"]
 
       configuration = {
         ProjectName = aws_codebuild_project.codebuild.name
       }
-   }
- }
+    }
+  }
 
- stage {
-   name = "Deploy"
-   action {
-     name             = "Deploy"
-      category         = "Deploy"
+  # Build stage for prod
+  stage {
+    name = "Build_Pord"
+    action {
+      name             = "Build_Pord"
+      category         = "Build"
       owner            = "AWS"
-      provider         = "ECS"
+      provider         = "CodeBuild"
       version          = "1"
-      input_artifacts  = ["build_output"]
+      input_artifacts  = ["source_output_prod"]
+      output_artifacts = ["build_output_prod"]
 
       configuration = {
-        ClusterName   = aws_ecs_cluster.cluster.name
-        ServiceName   = aws_ecs_service.service.name
-        FileName      = "imagedefinitions.json"
+        ProjectName = aws_codebuild_project.codebuild.name
       }
-   }
- }
- artifact_store {
-   type = "s3"
-   location = aws_s3_bucket.s3_artifact.bucket
- }
+    }
+  }
+  artifact_store {
+    type     = "s3"
+    location = aws_s3_bucket.s3_artifact.bucket
+  }
 }
-
-
- */
