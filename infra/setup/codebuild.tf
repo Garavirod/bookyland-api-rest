403a16c45450e32525ff50b729e2b80291baf5ee
Garavirod <rodrigogarciaavila26@gmail.com>
2024-09-10 11:57:22 -0600

    feat(infra | setup | codebuild): Move buildspec.yaml

--- infra/setup/codebuild.tf
+++ infra/setup/codebuild.tf
@@ -50,7 +50,7 @@ resource "aws_codebuild_project" "deploy_dev" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = file("buildspec/buildspec.yml")
+    buildspec = "buildspec.yml"
   }
 }
 
