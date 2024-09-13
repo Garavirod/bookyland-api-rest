1175b5b2eacfa729a98d2314a68687b1ce2280b7
Garavirod <rodrigogarciaavila26@gmail.com>
2024-09-12 21:30:01 -0600

    Add missing s3 bucket permissions into iam policy

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -136,7 +136,8 @@ data "aws_iam_policy_document" "s3" {
       "s3:GetObject",
       "s3:ListBucket",
       "s3:GetBucketPolicy",
-      "s3:GetBucketAcl"
+      "s3:GetBucketAcl",
+      "s3:GetBucketCORS"
     ]
     resources = [
       /* "arn:aws:s3:::${aws_s3_bucket.s3_artifact.bucket}",
