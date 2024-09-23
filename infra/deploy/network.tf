413d4b19c444753be7c12457b8e7e60160b21853
Garavirod <rodrigogarciaavila26@gmail.com>
2024-09-23 15:30:54 -0600

    feat(infra | deploy | network): Add vpc for testing

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -1,4 +1,4 @@
-/* #################
+#################
 # Network infra #
 #################
 
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
-
+/*
 ####################
 # Internet Gateway #
 ####################
